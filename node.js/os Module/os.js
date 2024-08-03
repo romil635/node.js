f56154a1e04ee116f9ac04dc08923d975bcceed8
romil635 <139975938+romil635@users.noreply.github.com>
2024-08-03 18:53:03 +0530

    3-8-24

---
+++ node.js/os Module/os.js
@@ -0,0 +1,27 @@
+// OS module
+const os = require('os');
+
+// console.log(os.EOL())
+// console.log(os.availableParallelism())
+// console.log(os.arch())
+// console.log(os.constants())
+// console.log(os.cpus())
+// console.log(os.devNull)
+// console.log(os.endianness())
+// console.log(os.freemem())
+// console.log(os.getPriority([pid])
+// console.log(os.homedir())
+// console.log(os.hostname())
+// console.log(os.loadavg())
+// console.log(os.networkInterfaces())
+// console.log(os.platform())
+// console.log(os.release())
+// console.log(os.setPriority([pid,] priority))
+// console.log(os.tmpdir())
+// console.log(os.totalmem())
+// console.log(os.type())
+// console.log(os.uptime())
+// console.log(os.userInfo([options]))
+// console.log(os.version())
+
+
